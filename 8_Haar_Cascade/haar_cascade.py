c0410915a2d86c7439972f4fd09a1e681960515c
yasin <yasn.866644@gmail.com>
2024-02-05 01:05:44 +0300

    Haar Cascade

---
+++ 8_Haar_Cascade/haar_cascade.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+# Geliştirici --> alfred Haar
+# Haar Cascade
+# Resim veya görüntülerin pikselleri üzerinde Haar-Like aranır
+# 1. İlgili kütüphaneyi, görüntüyü ve haar cascade dosyasını dahil et
+# 2. Görüntüyü gri tonlara çevirerek üzerinde ilgili nesneyi ara
+# 3. Bulunan nesneyi işaretle
